1ed9b1d04208845a3b29dc60b698edb636fb7c68
Anastacia Silva canto <ansilva-@c1r21s3.42lisboa.com>
2022-12-12 12:07:30 +0000

    refactoring echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ansilva- <ansilva-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:31:56 by ansilva-          #+#    #+#             */
-/*   Updated: 2022/12/09 15:05:28 by anastacia        ###   ########.fr       */
+/*   Updated: 2022/12/12 12:06:49 by ansilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,31 @@
 
 int	ft_echo(char *line)
 {
-	size_t	i;
+    int     option;
+    size_t  i;
 
-	i = 5;
-	if (i < ft_strlen(line) && line[i])
-	{
-		check_option(line, &i);
-		while (line[i])
-		{
-			if (line[i] == '$')
-			{
-				if (ft_expand_env(line, &i) == 1)
-					break ;
-			}
-			if (line[i])
-				printf("%c", line[i++]);
-		}
-	}
-	if (i >= ft_strlen(line) || check_option(line, NULL) == -1)
-		printf("\n");
-	return (0);
+    i = 5;
+    option = 0;
+	if (!ft_strncmp(line, "echo -n", 7))
+    {
+        option = 1;
+        i = 8;
+    }
+    if (i < ft_strlen(line))
+    {
+        while (line[i])
+        {
+            if (line[i] == '$')
+            {
+                if (ft_expand_env(line, &i) == 1)
+                    break ;
+            }
+            if (line[i])
+                printf("%c", line[i++]);
+        }
+    }
+    if (option == 0)
+        printf("\n");
+    return (0);
 }
 
-int	check_option(char *line, size_t *pos)
-{
-	int	i;
-
-	i = 5;
-	if (line[i] && line[i + 1] && line[i] == '-' && line[i + 1] == 'n')
-	{
-		if (*pos)
-			*pos = 8;
-		return (0);
-	}
-	return (-1);
-}
--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ansilva- <ansilva-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:41:46 by anastacia         #+#    #+#             */
-/*   Updated: 2022/12/09 17:24:25 by anastacia        ###   ########.fr       */
+/*   Updated: 2022/12/12 12:06:59 by ansilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ char	*fix_spaces(char *line);
 //Builtins
 void	to_builtins(char *line);
 int		ft_echo(char *line);
-int		check_option(char *line, size_t *pos);
 int		ft_exit(char *line);
 int		ft_expand_env(char *line, size_t *pos);
 int		ft_pwd(void);
