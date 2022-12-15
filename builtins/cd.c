92818f74ce0417eacf5345c8e896fe86418907ce
Anastacia Silva canto <ansilva-@c1r21s3.42lisboa.com>
2022-12-15 15:33:46 +0000

    fixing errors management on cd builtin

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ansilva- <ansilva-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 12:19:09 by ansilva-          #+#    #+#             */
-/*   Updated: 2022/12/12 12:24:36 by ansilva-         ###   ########.fr       */
+/*   Updated: 2022/12/15 15:30:33 by ansilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,21 @@ int	ft_cd(char *line)
 {
 	size_t	i;
 	char	*path;
+	char	*user;
 
 	i = 3;
 	if (i < ft_strlen(line))
 	{
 		path = ft_substr(line, i, ft_strlen(line) - i + 1);
-		chdir(path);
-		free (path);
+		if (chdir(path) == -1)
+			printf("minishell: cd: %s: No such file or directory\n", path);
 	}
+	else
+	{
+		user = getenv("USER");
+		path = ft_strjoin("/nfs/homes/", user);
+		chdir(path);
+	}
+	free (path);
 	return (0);
 }
