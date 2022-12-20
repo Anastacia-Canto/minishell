e0b7b193f5840f1e5f9ed4494ab85c3f3f4c14b0
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2022-12-20 18:16:45 +0000

    fixing rl

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 12:10:03 by ansilva-          #+#    #+#             */
-/*   Updated: 2022/12/20 03:14:48 by sde-mull         ###   ########.fr       */
+/*   Updated: 2022/12/20 18:15:15 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ft_exit(char *line)
 		ret = ft_atoi(args[1]);
 	free_array(args);
 	free (line);
-	clear_history();
+	rl_clear_history();
 	exit (ret % 256);
 }
 
@@ -40,7 +40,7 @@ int	check_exit_args(char **args, size_t len)
 	{
 		if (check_num(args[1]))
 		{
-			clear_history();
+			rl_clear_history();
 			free_array(args);
 			exit(2);
 		}
