fdb3783d645492ae466eb80a0d0109848139de70
Anastacia <anaanestesia@gmail.com>
2023-01-10 08:49:48 +0000

    pipe like in the paper

--- src/pipe.c
+++ src/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ansilva- <ansilva-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 15:27:36 by ansilva-          #+#    #+#             */
-/*   Updated: 2023/01/09 18:10:56 by ansilva-         ###   ########.fr       */
+/*   Updated: 2023/01/10 08:44:18 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,47 @@
 
 void	ft_pipe(char **cmds)
 {
-	// (void)cmds;
 	char	*adjusted_line;
+	int		tmpin;
+	int		tmpout;
+	int		fdin;
+	int		fdout;
 	int		fd[2];
-	pid_t	pid1;
-	pid_t	pid2;
+	pid_t	pid;
 	int		i;
-	
+
+	tmpin = dup(0);
+	tmpout = dup(1);
+	fdin = dup(tmpin);
 	i = 0;
-	pipe(fd);
-	// fd[1] = open("output.txt", O_RDWR | O_CREAT, 0777);
-	data()->pipe_flag = 1;
-	pid1 = fork();
-	//printf("waiting 2\n");
-	if (pid1 == 0)
+	while (i < array_len(cmds))
 	{
-		adjusted_line = adjust_line(cmds[i]);
-		// printf("input %s\n", cmds[i]);
-		adjusted_line = check_if_env(adjusted_line);
-		to_builtins(adjusted_line, 1);
-		dup2(fd[1], STDOUT_FILENO);
-		free (adjusted_line);
-		close(fd[0]);
-		close(fd[1]);
-		// execlp("ping", "ping","-c", "5", "google.com", NULL);
-		exit(0);
+		dup2(fdin, STDIN_FILENO);
+		close(fdin);
+		if (i == array_len(cmds) - 1)
+			fdout = dup(tmpout);
+		else
+		{
+			pipe(fd);
+			fdout = fd[1];
+			fdin = fd[0];
+		}
+		dup2(fdout, 1);
+		close (fdout);
+		pid = fork();
+		if (pid == 0)
+		{
+			adjusted_line = adjust_line(cmds[i]);
+			adjusted_line = check_if_env(adjusted_line);
+			to_builtins(adjusted_line, 1);
+			free (adjusted_line);
+			exit(0);
+		}
+		i++;
 	}
-	pid2 = fork();
-	if (pid2 == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		adjusted_line = adjust_line(cmds[++i]);
-		// printf("input %s\n", cmds[i]);
-		adjusted_line = check_if_env(adjusted_line);
-		
-		to_builtins(adjusted_line, 1);
-		free (adjusted_line);
-		close(fd[0]);
-		close(fd[1]);
-		// execlp("grep", "grep", "rtt", NULL);
-		exit(0);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
-	printf("waiting 2\n");
-	return ;
+	dup2(tmpin, 0);
+	dup2(tmpout, 1);
+	close(tmpin);
+	close(tmpout);
+	waitpid(pid, NULL, 0);
 }
