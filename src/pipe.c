34f9e2af3006633830e74aeee79198d48e8e13e8
Anastacia <anaanestesia@gmail.com>
2023-02-02 12:10:23 +0000

    fixed pipe

--- src/pipe.c
+++ src/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 15:27:36 by ansilva-          #+#    #+#             */
-/*   Updated: 2023/01/28 21:46:26 by sde-mull         ###   ########.fr       */
+/*   Updated: 2023/02/02 11:18:46 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ int	ft_pipe(char ***cmds, int len)
 	int		tmpout;
 	int		fd[2];
 	int		p1[2];
+	int		fdpipe[2];
 	pid_t	pid;
 	int		i;
 
 	tmpin = dup(0);
 	tmpout = dup(1);
+	fd[0] = dup(tmpin);
 	i = -1;
 	while (++i < len)
 	{
-		dup2(fd[0], STDIN_FILENO);
+		dup2(fd[0], 0);
 		close(fd[0]);
 		if (i == len - 1)
 		{
@@ -34,7 +36,11 @@ int	ft_pipe(char ***cmds, int len)
 			pipe(p1);
 		}
 		else
-			pipe(fd);
+		{
+			pipe(fdpipe);
+			fd[1] = fdpipe[1];
+			fd[0] = fdpipe[0];
+		}
 		dup2(fd[1], 1);
 		close (fd[1]);
 		pid = fork();
@@ -50,13 +56,13 @@ int	ft_pipe(char ***cmds, int len)
 			exit(data()->exit_status);
 		}
 	}
-	close(p1[1]);
-	read(p1[0], &data()->exit_status, sizeof(int));
-	close(p1[0]);
 	dup2(tmpin, 0);
 	dup2(tmpout, 1);
 	close(tmpin);
 	close(tmpout);
+	close(p1[1]);
+	read(p1[0], &data()->exit_status, sizeof(int));
+	close(p1[0]);
 	waitpid(pid, NULL, 0);
 	return (data()->exit_status);
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-echo hello world
+cat test.sh
