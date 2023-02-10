4a4b4d4182b7296508f74be2d6b2afdc7e1b4a59
Anastacia <anaanestesia@gmail.com>
2023-02-10 16:50:45 +0000

    refactoring pipe first good step

--- src/pipe.c
+++ src/pipe.c
@@ -6,7 +6,7 @@
 /*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 15:27:36 by ansilva-          #+#    #+#             */
-/*   Updated: 2023/02/10 11:54:58 by anastacia        ###   ########.fr       */
+/*   Updated: 2023/02/10 16:50:22 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ int	ft_pipe(char ***cmds, int len)
 	int		fd[2];
 	int		p1[2];
 	int		fdpipe[2];
-	pid_t	pid;
 	int		i;
+	int		sig;
 
 	tmpin = dup(0);
 	tmpout = dup(1);
 	fd[0] = dup(tmpin);
 	i = -1;
+	sig = 0;
 	while (++i < len)
 	{
 		dup2(fd[0], 0);
@@ -34,6 +35,7 @@ int	ft_pipe(char ***cmds, int len)
 		{
 			fd[1] = dup(tmpout);
 			pipe(p1);
+			sig = 1;
 		}
 		else
 		{
@@ -43,18 +45,7 @@ int	ft_pipe(char ***cmds, int len)
 		}
 		dup2(fd[1], 1);
 		close (fd[1]);
-		pid = fork();
-		if (pid == 0)
-		{
-			execution(cmds[i], p1);
-			if (i == len - 1)
-			{
-				close(p1[0]);
-				write(p1[1], &data()->exit_status, sizeof(int));
-				close(p1[1]);
-			}
-			exit(data()->exit_status);
-		}
+		ft_execute_pipe(cmds[i], p1, sig);
 	}
 	dup2(tmpin, 0);
 	dup2(tmpout, 1);
@@ -63,6 +54,24 @@ int	ft_pipe(char ***cmds, int len)
 	close(p1[1]);
 	read(p1[0], &data()->exit_status, sizeof(int));
 	close(p1[0]);
-	waitpid(pid, NULL, 0);
 	return (data()->exit_status);
+}
+
+void	ft_execute_pipe(char **cmds, int *p1, int sig)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execution(cmds, p1);
+		if (sig)
+		{
+			close(p1[0]);
+			write(p1[1], &data()->exit_status, sizeof(int));
+			close(p1[1]);
+		}
+		exit(data()->exit_status);
+	}
+	waitpid(pid, NULL, 0);
 }
