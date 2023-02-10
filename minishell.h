ed20009bc44942766bfaddc0aeff0646abecdce4
Anastacia <anaanestesia@gmail.com>
2023-02-10 17:00:14 +0000

    refactoring pipe - almost there

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:41:46 by anastacia         #+#    #+#             */
-/*   Updated: 2023/02/10 16:44:44 by anastacia        ###   ########.fr       */
+/*   Updated: 2023/02/10 16:59:49 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ void	to_builtins(char **line, int *pd);
 int		treat_others(char **line, int fd);
 //Pipe---------------------------------------------------------------
 int		ft_pipe(char ***cmds, int len);
-void	ft_execute_pipe(char **cmds, int *p1, int sig);
+void	ft_execute_pipe(char **cmds, int i, int len);
 //Exec---------------------------------------------------------------
 int		ft_exec(char **line, int file);
 int		exec_prog(char **line, int *fd);
--- src/pipe.c
+++ src/pipe.c
@@ -6,7 +6,7 @@
 /*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 15:27:36 by ansilva-          #+#    #+#             */
-/*   Updated: 2023/02/10 16:50:22 by anastacia        ###   ########.fr       */
+/*   Updated: 2023/02/10 16:58:23 by anastacia        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,19 @@ int	ft_pipe(char ***cmds, int len)
 	int		tmpin;
 	int		tmpout;
 	int		fd[2];
-	int		p1[2];
 	int		fdpipe[2];
 	int		i;
-	int		sig;
 
 	tmpin = dup(0);
 	tmpout = dup(1);
 	fd[0] = dup(tmpin);
 	i = -1;
-	sig = 0;
 	while (++i < len)
 	{
 		dup2(fd[0], 0);
 		close(fd[0]);
 		if (i == len - 1)
-		{
 			fd[1] = dup(tmpout);
-			pipe(p1);
-			sig = 1;
-		}
 		else
 		{
 			pipe(fdpipe);
@@ -45,33 +38,39 @@ int	ft_pipe(char ***cmds, int len)
 		}
 		dup2(fd[1], 1);
 		close (fd[1]);
-		ft_execute_pipe(cmds[i], p1, sig);
+		ft_execute_pipe(cmds[i], i, len);
 	}
 	dup2(tmpin, 0);
 	dup2(tmpout, 1);
 	close(tmpin);
 	close(tmpout);
-	close(p1[1]);
-	read(p1[0], &data()->exit_status, sizeof(int));
-	close(p1[0]);
 	return (data()->exit_status);
 }
 
-void	ft_execute_pipe(char **cmds, int *p1, int sig)
+void	ft_execute_pipe(char **cmds, int i, int len)
 {
 	int	pid;
+	int	p1[2];
 
+	if (i == len - 1)
+		pipe(p1);
 	pid = fork();
 	if (pid == 0)
 	{
 		execution(cmds, p1);
-		if (sig)
+		if (i == len - 1)
 		{
 			close(p1[0]);
 			write(p1[1], &data()->exit_status, sizeof(int));
 			close(p1[1]);
 		}
 		exit(data()->exit_status);
+	}
+	if (i == len - 1)
+	{
+		close(p1[1]);
+		read(p1[0], &data()->exit_status, sizeof(int));
+		close(p1[0]);
 	}
 	waitpid(pid, NULL, 0);
 }
